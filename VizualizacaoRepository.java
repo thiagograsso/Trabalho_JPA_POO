dd011ec57f6a9b53441cf9bb1aaf1f8018b804b4
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2025-09-26 19:31:36 -0300

    Create VizualizacaoRepository.java

---
+++ VizualizacaoRepository.java
@@ -0,0 +1,6 @@
+package com.streaming.repository;
+
+import com.streaming.entity.Visualizacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisualizacaoRepository extends JpaRepository<Visualizacao, Integer> { }

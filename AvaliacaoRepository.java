0321cb77eca24168fe24146ff6a3657679bb01dc
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2025-09-26 19:32:18 -0300

    Create AvaliacaoRepository.java

---
+++ AvaliacaoRepository.java
@@ -0,0 +1,6 @@
+package com.streaming.repository;
+
+import com.streaming.entity.Avaliacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Integer> { }

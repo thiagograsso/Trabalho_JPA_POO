599deb7f2cea4fe5cc241a20abaa5ca196d244ad
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2025-09-26 19:30:49 -0300

    Create PerfilRepository.java

---
+++ PerfilRepository.java
@@ -0,0 +1,6 @@
+package com.streaming.repository;
+
+import com.streaming.entity.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerfilRepository extends JpaRepository<Perfil, Integer> { }
